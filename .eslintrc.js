703f81d3fd2580a941a044b580f8049de23f0382
Alexey Goloburdin <sterx@rl6.ru>
2022-07-10 13:58:29 +0300

    init

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:solid/typescript",
+    "prettier",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint", "solid"],
+  rules: {},
+}
---
+++ setpath.sh
@@ -0,0 +1,2 @@
+# Add Node's local bin directory in $PATH — for convinient use local eslint, prettier
+export PATH=$(pwd)/node_modules/.bin:$PATH
